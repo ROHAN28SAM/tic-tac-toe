c54159fb8e789ecea9cbb3f977269c549127f644
ROHAN28SAM <rohan.sambhudas@gmail.com>
2021-08-03 16:43:59 +0530

    Initial Commit

---
+++ player.py
@@ -0,0 +1,40 @@
+import math
+import random
+
+class player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self,game):
+        pass
+
+
+class computer_player(player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+class human_player(player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+
+        while not valid_square:
+            square = input(self.letter + '\'s turn.Input move (0-8): ')
+
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            
+            except ValueError:
+                print("Invalid square, Try Again")
+        return val
+        
